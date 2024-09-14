81c456f02a2ed26e602466339d949b251acd8476
Nicoller-24 <166450090+Nicoller-24@users.noreply.github.com>
2024-09-14 02:26:13 +0000

    parte visual corregida

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,32 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+	<div className="container" style={{ backgroundColor: "white", width: "70%", paddingBottom: "10%" }}>
+            <h1 style={{ marginLeft: "30%" }}>Add a new contact</h1>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="Fullname" className="form-label">Full name</label>
+                    <input type="text" className="form-control" id="Fullname" placeholder="Full name"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="Email" placeholder="Email" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Phone" className="form-label">Phone</label>
+                    <input type="text" className="form-control" id="Phone" placeholder="Phone" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Adress" className="form-label">Address</label>
+                    <input type="text" className="form-control" id="Adress" placeholder="Adress" />
+                </div>
+                <button type="submit" className="btn btn-primary w-100 mb-4">Save</button>
+            </form>
+			<Link to="/demo">
+				<a href="#">or get back to contacts</a>
+			</Link>
+            
+        </div>
 );
